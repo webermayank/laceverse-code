33292a87854268b23879d353dcdb277f1efb0f46
webermayank <111176033+webermayank@users.noreply.github.com>
2025-02-07 21:17:56 +0530

    websoccket

--- apps/http/src/middleware/user.ts
+++ apps/http/src/middleware/user.ts
@@ -1,28 +1,35 @@
-
-
-import jwt  from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config";
+import { NextFunction, Request, Response } from "express";
 
+export const userMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const header = req.headers["authorization"];
+  const token = header?.split(" ")[1];  
+  console.log("Authorization Header:", header); // Debugging line
+  console.log("Extracted Token:", token); // Debugging line
+  console.log(req.route.path);
+  console.log(token);
 
+  if (!token) {
+    res.status(403).json({ message: "Unauthorized not token" });
+    return;
+  }
 
-export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const header = req.headers.authorization;
-    const token = header?.split(" ")[1]; //this converts "Bearer token" to [Bearer, token] AND GET 1st element
-    //   console.log("4444444=4=4=4=4==4=4=4=4");
-    //   console.log(token);
-    if (!token) {
-        res.status(403).json({ message: "invalid token" });
-        return;
-    }
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET) as { role: string, userId : string };
-        req.userId = decoded.userId;
-        
-        next();
-    }
-     catch (error) {
-        res.status(403).json({ message: "invalid token" });
-        return;
-    }
-}
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as {
+      role: string;
+      userId: string;
+    };
+    req.userId = decoded.userId;
+    console.log("Decoded User ID:", req.userId); // Debugging line
+
+    next();
+  } catch (e) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+};
