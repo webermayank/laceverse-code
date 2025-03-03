2b79cf914f5c8ad5330d6262f4ac341019a5339a
webermayank <111176033+webermayank@users.noreply.github.com>
2025-03-03 23:30:28 +0530

    added game assets in frontend

---
+++ apps/fe/src/game-assets/doors_windows.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="doors_windows" tilewidth="16" tileheight="16" tilecount="180" columns="18">
+ <image source="../../../../../Downloads/Top-Down_Retro_Interior/TopDownHouse_DoorsAndWindows.png" width="288" height="160"/>
+</tileset>
---
+++ apps/fe/src/game-assets/sprout-water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="sprout-water" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="../../../../../Downloads/Sprout Lands - Sprites - Basic pack/Sprout Lands - Sprites - Basic pack/Tilesets/Water.png" width="64" height="16"/>
+</tileset>
